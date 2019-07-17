422cbe79c0d27b407aa2ef2fad796cad76a0428c
saumya <2saumyaray@gmail.com>
2019-07-17 13:31:09 +0530

    WeatherComponent Data flow fixing

--- src/App.js
+++ src/App.js
@@ -14,61 +14,12 @@ import WeatherComp from './comps/Weather.comp';
 function App() {
 
   //console.log('process.env.NODE_ENV=',process.env.NODE_ENV);
-
-  function getWeatherInfo(){
-    console.log('getWeatherInfo');
-
-    const wCity = 'Bhubaneswar';
-    const appConfig = AppConfig();
-    const wURL = appConfig.api.weather.wURL+'&q='+ wCity +'&units=metric';
-    
-    //fetch(wURL).then(function(){}).catch(function(){});
-    fetch(wURL).then(function(result){
-      //console.log(result);
-      console.log('Result Status = '+result.status);
-      console.log('Status Info = '+result.statusText);
-      console.log('Result Type = '+result.type);
-      result.json().then(function(jResult){
-        //console.log(jResult);
-        if(jResult.cod===401){ 
-          console.log('Details : '+jResult.message) 
-        }else{
-          console.log(jResult);
-          console.log('==================== '+ wCity +' ==============');
-          console.log('Temperature='+jResult.main.temp,'Humidity='+jResult.main.humidity);
-          console.log('===============================================');
-        }
-      });
-    }).catch(function(error){
-      console.log('Error!');
-      console.log(error);
-      //console.log('Error Details: '+error.message);
-    });
-  }
+  const appConfig = AppConfig();
+  
   return (
     <React.Fragment>
       <CssBaseline />
-      {/*
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload!?!
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-      */}
-
-        <WeatherComp apiFrom='Open Weather' getWeatherInfo={getWeatherInfo} />
-        
+      <WeatherComp apiFrom='Open Weather' appConfig={appConfig} weatherConfig={appConfig.api.weather} />  
     </React.Fragment>
   );
 }
--- src/comps/Weather.comp.js
+++ src/comps/Weather.comp.js
@@ -7,6 +7,41 @@ import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 
 function WeatherComp(props){
+
+    //console.log('WeatherComp: process.env.NODE_ENV=',process.env.NODE_ENV);
+
+    function getWeatherInfo(){
+        console.log('WeatherComp:getWeatherInfo');
+    
+        const wCity = 'Bhubaneswar';
+        //const appConfig = props.appConfig;
+        //const wURL = appConfig.api.weather.wURL+'&q='+ wCity +'&units=metric';
+        const wURL = props.weatherConfig.wURL+'&q='+ wCity +'&units=metric';
+        
+        //fetch(wURL).then(function(){}).catch(function(){});
+        fetch(wURL).then(function(result){
+          //console.log(result);
+          console.log('Result Status = '+result.status);
+          console.log('Status Info = '+result.statusText);
+          console.log('Result Type = '+result.type);
+          result.json().then(function(jResult){
+            //console.log(jResult);
+            if(jResult.cod===401){ 
+              console.log('Details : '+jResult.message) 
+            }else{
+              console.log(jResult);
+              console.log('==================== '+ wCity +' ==============');
+              console.log('Temperature='+jResult.main.temp,'Humidity='+jResult.main.humidity);
+              console.log('===============================================');
+            }
+          });
+        }).catch(function(error){
+          console.log('Error!');
+          console.log(error);
+          //console.log('Error Details: '+error.message);
+        });
+      }
+
     return(
         <React.Fragment>
             <div className='compWeather'>WeatherComp using {props.apiFrom}.</div>
@@ -15,7 +50,7 @@ function WeatherComp(props){
                 Welcome to weather now.
                 </Typography>
                 
-                <Button color="primary" size="medium" variant="contained" onClick={ props.getWeatherInfo }>
+                <Button color="primary" size="medium" variant="contained" onClick={ getWeatherInfo }>
                 Get Weather info
                 </Button>
                 
